c04fa0b5b3a75e54ae80532115c475cb88789b63
Martín Miranda <debianitram@gmail.com>
2017-08-12 00:49:38 -0300

    Se agregó a la función de migración csv registros por defecto para Tutor/ Curso

--- project/migracion.py
+++ project/migracion.py
@@ -5,25 +5,47 @@ def borra_todo():
     models.Asistente.objects.all().delete()
 
 def cargar_asistentes():
-    ifile  = open('asistentes.csv', "r")
-    read = csv.reader(ifile)
-    cur = models.Curso.objects.get(pk=1)
-    for row in read :
-        nombre_completo = row[2].title().split()
+    with open('asistentes.csv', "r") as ifile:
+        read = csv.reader(ifile)
+        
+        # Obtener docente para el curso.
+        if not models.Tutor.objects.exists():
+            docente = models.Tutor.objects.create(
+                nombre='Docente',
+                apellido='Workshop Git',
+                dni='00000001'
+            )
+        else:
+            docente = models.Tutor.objects.first()
 
-        apellido = nombre_completo[-1]
-        nombre = ' '.join(nombre_completo[0:-1])
-        dni = row[3]
-        institucion = row[4]
-        email = row[1]
-        # carrera = row[5]
-        # mu = row[6]
+        # Obtener curso para los asistentes.
+        if not models.Curso.objects.exists():
+            curso = models.Curso.objects.create(
+                nombre='Workshop Git & Github',
+            )
 
-        models.Asistente.objects.create(
-            apellido=apellido, 
-            nombre=nombre, 
-            dni=dni,
-            email=email, 
-            institucion=institucion, 
-            curso=cur)
-        print(nombre_completo)
+            curso.tutor.add(docente)
+
+        else:
+            curso = models.Curso.objects.first()
+
+
+        for row in read :
+            nombre_completo = row[2].title().split()
+
+            apellido = nombre_completo[-1]
+            nombre = ' '.join(nombre_completo[0:-1])
+            dni = row[3]
+            institucion = row[4]
+            email = row[1]
+
+            asistente = models.Asistente.objects.create(
+                apellido=apellido, 
+                nombre=nombre, 
+                dni=dni,
+                email=email, 
+                institucion=institucion, 
+                curso=curso
+            )
+            
+            print('Se agregó al asistente: %s' % asistente)
